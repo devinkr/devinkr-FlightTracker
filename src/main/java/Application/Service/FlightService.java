f9fcfd398aa8d159bb4ee033b07a46ef964f7d22
Devin Raleigh <devinkr@gmail.com>
2023-02-21 19:23:03 +0000

    Complete FlightDAO and FlightService

--- src/main/java/Application/Service/FlightService.java
+++ src/main/java/Application/Service/FlightService.java
@@ -53,7 +53,8 @@ public class FlightService {
      *         inform our provide the front-end client with information about the added Flight.
      */
     public Flight addFlight(Flight flight){
-        return null;
+        Flight newFlight = flightDAO.insertFlight(flight);
+        return newFlight;
     }
 
     /**
@@ -70,7 +71,12 @@ public class FlightService {
      *         user should have some insight if they attempted to edit a nonexistent flight.)
      */
     public Flight updateFlight(int flight_id, Flight flight){
-        return null;
+        Flight foundFlight = flightDAO.getFlightById(flight_id);
+        if (foundFlight == null) {
+            return null;
+        }
+        flightDAO.updateFlight(flight_id, flight);
+        return foundFlight;
     }
 
     /**
@@ -80,7 +86,8 @@ public class FlightService {
      * @return all flights in the database.
      */
     public List<Flight> getAllFlights() {
-        return null;
+        List<Flight> flights = flightDAO.getAllFlights();
+        return flights;
     }
 
     /**
@@ -92,6 +99,8 @@ public class FlightService {
      * @return all flights departing from departure_city and arriving at arrival_city.
      */
     public List<Flight> getAllFlightsFromCityToCity(String departure_city, String arrival_city) {
-        return null;
+
+        List<Flight> flights = flightDAO.getAllFlightsFromCityToCity(departure_city, arrival_city);
+        return flights;
     }
 }
